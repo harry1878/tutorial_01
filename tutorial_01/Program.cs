93870fa2c1768703d457050d90b69a718d5cecd0
harry1878 <60026408+harry1878@users.noreply.github.com>
2020-01-18 17:02:53 +0900

    D+1

--- tutorial_01/Program.cs
+++ tutorial_01/Program.cs
@@ -1,15 +1,137 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace tutorial_01
 {
     class Program
     {
+        // 열거형 (Enum)
+        enum Weapon
+        {
+            None = 0,
+            Longsword,
+            staff,
+            Bow,
+            Etc
+        }
         static void Main(string[] args)
         {
+            #region 조건문 및 반복문
+
+
+            // ctrl + shift +b 전체 빌드 단축키
+            //ctrl + F5 빌드 후 실행 
+            // int -21억 부터 21억 까지 들어감 
+            // float 부동 소수점. 소수점 7자리(6자리)
+            // double 부동 소수점. 소수점 16자리(12자리)
+            // bool ture 또는 false 값만 들어가는 자료형
+            // string 문자열을 넣을 수 있는 자료형
+            // char -126~ 126
+            // unsinged char 0~256 
+
+            //int value = 0;
+            //value = value + 1;
+            //value += 1;
+
+            //value = value - 1;
+            //value -= 1;
+
+            //value = value * 1;
+            //value *= 1;
+
+            //value = value / 1;
+            //value /= 1;
+
+            ////조건문
+            //if(value  ==1)
+            //{
+            //    Console.WriteLine("ok");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("false");
+            //}
+            ////반복문
+            ////for,while
+            //for (int i = 0; i <= 10; ++i)
+            //{
+            //    Console.WriteLine(i.ToString());
+            //    // printf("%d\n",i);
+            //}
+            // 전체 주석하기 
+            // ctrl + k + c
+            // 주석 풀기 
+            // ctrl + k + u
+            // i는 0부터 5까지 출력하되 
+            //for (int i = 0; i <= 5; ++i)
+            //{
+            //    Console.WriteLine(i);
+            //    if (i == 2) Console.WriteLine("OK");
+            //    else Console.WriteLine("False");
+
+            //}
+            ////구구단 
+            ////1단부터 9단
+            //for(int i = 1; i <=9; i++)
+            //{
+            //    for (int j = 1; j <= 9; j++)
+            //    {
+            //        Console.WriteLine("{0} x {1} = {2}",i, j, i * j);
+            //    }
+            //}
+            #endregion
+
+            //switch 
+            //조건문
+
+            //int money = 100000;
+            //// 내가 지금 장착 하고 있는무기
+
+            //Weapon weapon = Weapon.None;
+            ////weapon = default(Weapon);
+
+            //string str = string.Empty;
+            //Console.ReadLine();
+
+            //Console.WriteLine("내가 장착한 무기:"+ weapon.ToString());
+            //Weapon findWeapon = Weapon.None;
+            //Enum.TryParse(str, out findWeapon);
+
+            ////case, break, default
+            //switch (findWeapon)
+            //{
+            //    case Weapon.Longsword:
+            //        weapon = Weapon.Longsword;
+            //        break;
+
+            //    case Weapon.staff:
+            //        weapon = Weapon.Bow;
+            //        break;
+
+            //    default:
+            //        weapon = Weapon.Etc;
+            //        break;
+
+
+
+
+            //}
+            //Console.WriteLine("새로 장착한 무기:"+ weapon.ToString());
+
+            //while
+            //반복문
+
+            int value = 0;
+
+            while (value != 100)
+            {
+                value++;
+                Console.WriteLine(value.ToString());
+
+            }
+           
         }
     }
 }
---
+++ vending Machine/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+
+namespace vending_Machine
+{
+    class Program
+    {
+        enum product
+        {
+            None = 0,
+            Cola,
+            Sida,
+            Fanta,
+            Water,
+            Coffee,
+            Tea
+        }
+
+        static void Main(string[] args)
+        {
+            // 자판기 
+            int money = 10000;
+
+            string input = "Start";
+            while (!string.IsNullOrEmpty(input))
+            {
+                
+                Console.Clear();
+                Console.WriteLine("잔여액:{0}", money);
+                Console.WriteLine("어서오세요 자판기 입니다");
+                // 1. 상품 보여주기!
+                
+        
+
+        
+
+                Console.WriteLine("상품 종류: Cola(100), Sida(200), Fanta(300), Water(1000), Coffee(1), Tea(30)");
+
+
+                // 2. 입력 받기
+                Console.WriteLine("사고 싶은 상품을 입력해주세요");
+                
+                input = Console.ReadLine();
+
+                product findproduct = product.None;
+                Enum.TryParse(input, out findproduct);
+
+
+
+                // Cola(100), Sida(200), Fanta(300), Water(1000), Coffee(1), Tea(30)
+
+
+                // 3. 계산하기
+                if (findproduct == product.Tea)
+                {
+                    if (money >= 30)
+                    {
+                        money = money - 30;
+                       
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("돈이 부족하여 이 상품을 구입할 수 없습니다.");
+                        Thread.Sleep(1000);
+                    }
+
+                }
+                if (findproduct == product.Coffee)
+                {
+                    if (money >= 1)
+                    {
+                        money = money - 1;
+                       
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("돈이 부족하여 이 상품을 구입할 수 없습니다.");
+                        Thread.Sleep(1000);
+                    }
+
+                }
+                if (findproduct == product.Water)
+                {
+                    if (money >= 1000)
+                    {
+                        money = money - 1000;
+                        
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("돈이 부족하여 이 상품을 구입할 수 없습니다.");
+                        Thread.Sleep(1000);
+                    }
+
+                }
+                if (findproduct == product.Fanta)
+                {
+                    if (money >= 300)
+                    {
+                        money = money - 300;
+                        
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("돈이 부족하여 이 상품을 구입할 수 없습니다.");
+                        Thread.Sleep(1000);
+                    }
+
+                }
+                if (findproduct == product.Sida)
+                {
+                    if (money >= 200)
+                    {
+                        money = money - 200;
+                        
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("돈이 부족하여 이 상품을 구입할 수 없습니다.");
+                        Thread.Sleep(1000);
+                    }
+
+                }
+
+                if (findproduct == product.Cola)
+                {
+                    if (money >= 100)
+                    {
+                        money = money - 100;
+                        
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("돈이 부족하여 이 상품을 구입할 수 없습니다.");
+                        Thread.Sleep(1000);
+                    }
+
+                }
+
+
+            }
+
+            Console.WriteLine("자판기 종료!");  
+
+
+        }
+    }
+}
